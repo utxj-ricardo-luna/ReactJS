243fe6139c2cbaaed8b7c4403c3d406491272ae6
Marco A. Ramírez Hernández <marco.ramirez@utxicotepec.edu.mx>
2026-02-12 14:33:02 -0600

    Add componente googleMaps to react

--- my-react-app/src/Contacto.jsx
+++ my-react-app/src/Contacto.jsx
@@ -1,7 +1,45 @@
+import './Contacto.css'
 function Contacto() {
     return (
         <div className="contactoDiv">
-            <h2>Contacto</h2>
+            <h2 className='titform'>Contacto</h2>
+            <form>
+                <div>
+                    <label>Nombre:</label>
+                    <input
+                        type="text"
+                        name="nombre"
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label>Correo Electrónico:</label>
+                    <input
+                        type="email"
+                        name="correo"
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label>Teléfono:</label>
+                    <input
+                        type="tel"
+                        name="telefono"
+                    />
+                </div>
+
+                <div>
+                    <label>Asunto:</label>
+                    <textarea
+                        name="asunto"
+                        required
+                    />
+                </div>
+
+                <button type="submit">Enviar</button>
+            </form>
         </div>
     );
 }
--- my-react-app/src/Sucursales.jsx
+++ my-react-app/src/Sucursales.jsx
@@ -1,8 +1,32 @@
+import Mapa from './Mapa';
+import './Sucursales.css'
 function Sucursales() {
     return (
+        <>
         <div className="sucursalesDiv">
-            <h2>Sucursales</h2>
+        <TarjetaSucursal nombre="ServiTec Centro" direccion = "Calle 3 de Marzo No 23, Col. Centro Xicotepec, Puebla México" telefono="+52 7647643322" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png" latitud=20.267235359887085 longitud=-97.95997531836774 />
+        <TarjetaSucursal nombre="ServiTec Tierra Negra" direccion = "Av. Univesidad Tecnologica No 1000 Col. Tierra Negra Xicotepec, Puebla, México" telefono="+52 7647648722" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
+        <TarjetaSucursal nombre="ServiTec La rivera" direccion = "Av. Independencia No 23 Col. La Rivera Xicotepec, Puebla México" telefono="+52 7647646032" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
+        <TarjetaSucursal nombre="ServiTec El Tabacal" direccion = "17 de Junio Norte 654 Col. El Tabacla Xicotepec, Puebla, México" telefono="+52 7647641147" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
         </div>
+        </>
     );
 }
+function TarjetaSucursal(props){
+    return(
+        <div className='tarjetaDiv'>
+            <img src={props.imagen} alt='Logotipo'/>
+            <h3>{props.nombre}</h3>
+            <p>{props.direccion}</p>
+            <Mapa 
+                lat = {props.latitud}
+                lng= {props.longitud}
+                nombre_sucursal={props.nombre}
+            />
+            <p>{props.telefono}</p>
+            <a href='#'>Ver más</a>
+        </div>
+    )
+}
+
 export default Sucursales;
