fa80569987d7157f4e54772e9806422ab5eb53cd
Marco A. Ramírez Hernández <marco.ramirez@utxicotepec.edu.mx>
2026-02-13 14:17:22 -0600

    Create component ubication

--- my-react-app/src/PromosContenido.jsx
+++ my-react-app/src/PromosContenido.jsx
@@ -1,15 +1,11 @@
-import Mapa from './Mapa';
+import MapaGeolocalizacion from './MapaGeolocalizacion';
 import './PromosContenido.css';
 function PromosContenido(){
     return(
         <div className="promosContenido">
             <h2>Promociones</h2>
             <p>Descubre nuestras ofertas exclusivas y promociones especiales diseñadas para brindarte el mejor valor en tus compras. ¡Aprovecha estas oportunidades únicas antes de que se agoten!</p>
-            <Mapa 
-                lat = {20.267235359887085}
-                lng= {-97.95997531836774}
-                nombre_sucursal={"Centro Xicotepec"}
-            />
+            <MapaGeolocalizacion/>
         </div>
     )
 }
--- my-react-app/src/Sucursales.jsx
+++ my-react-app/src/Sucursales.jsx
@@ -4,10 +4,40 @@ function Sucursales() {
     return (
         <>
         <div className="sucursalesDiv">
-        <TarjetaSucursal nombre="ServiTec Centro" direccion = "Calle 3 de Marzo No 23, Col. Centro Xicotepec, Puebla México" telefono="+52 7647643322" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png" latitud=20.267235359887085 longitud=-97.95997531836774 />
-        <TarjetaSucursal nombre="ServiTec Tierra Negra" direccion = "Av. Univesidad Tecnologica No 1000 Col. Tierra Negra Xicotepec, Puebla, México" telefono="+52 7647648722" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
-        <TarjetaSucursal nombre="ServiTec La rivera" direccion = "Av. Independencia No 23 Col. La Rivera Xicotepec, Puebla México" telefono="+52 7647646032" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
-        <TarjetaSucursal nombre="ServiTec El Tabacal" direccion = "17 de Junio Norte 654 Col. El Tabacla Xicotepec, Puebla, México" telefono="+52 7647641147" imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"/>
+        <TarjetaSucursal 
+        nombre="ServiTec Centro"
+        direccion = "Calle 3 de Marzo No 23, Col. Centro Xicotepec, Puebla México"
+        telefono="+52 7647643322"
+        imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"
+        latitud = {20.276611665977292}
+        longitud = {-97.95782178080484}
+        />
+
+
+        <TarjetaSucursal
+        nombre="ServiTec Tierra Negra"
+        direccion = "Av. Univesidad Tecnologica No 1000 Col. Tierra Negra Xicotepec, Puebla, México"
+        telefono="+52 7647648722"
+        imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"
+        latitud = {20.244063827889793 }
+        longitud = {-97.95719862403506}
+        />
+        <TarjetaSucursal 
+        nombre="ServiTec La rivera"
+        direccion = "Av. Independencia No 23 Col. La Rivera Xicotepec, Puebla México"
+        telefono="+52 7647646032"
+        imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"
+        latitud = {20.265291182072218 }
+        longitud = {-97.9680256853732}
+        />
+        <TarjetaSucursal
+        nombre="ServiTec El Tabacal"
+        direccion = "17 de Junio Norte 654 Col. El Tabacla Xicotepec, Puebla, México"
+        telefono="+52 7647641147"
+        imagen="https://servitecingenieros.com/wp-content/uploads/2023/11/Logo-Cicular-Borde-Azul-Servitec.png"
+        latitud = {20.286462780285625}
+        longitud = {-97.95445587443318}
+        />
         </div>
         </>
     );
